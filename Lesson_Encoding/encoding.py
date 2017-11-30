bfb2336ae30af532884c614d8e3186e57707f625
Vasily <33507763+VasilyMagay@users.noreply.github.com>
2017-11-30 21:55:53 +0300

    to PEP8

--- Lesson_Encoding/encoding.py
+++ Lesson_Encoding/encoding.py
@@ -1,35 +1,38 @@
 import chardet
 
-def get_dict_from_news_file(filename, cp='', min_word_len = 6):
+
+def get_dict_from_news_file(filename, min_word_len=6):
     
-    dict = {}
-    text = ""
-    
+    my_dict = {}
+
     with open(filename, 'rb') as f:
         data = f.read()
         result = chardet.detect(data)
-        text = data.decode(result['encoding'])
+        text_data = data.decode(result['encoding'])
         
-    if len(text) > 0:    
-        line = text.strip()
+    if text_data:
+        line = text_data.strip()
         line_list = line.split(' ')
         for word in line_list:
             if len(word) >= min_word_len:
                 word = word.lower()
-                dict[word] = dict.get(word, 0) + 1
+                my_dict[word] = my_dict.get(word, 0) + 1
      
-    return dict
-    
-def print_top(dict, top_num=10):
-    sorted_list = sorted(dict.items(), key=lambda x: x[1], reverse=True)
+    return my_dict
+
+
+def print_top(my_dict, top_num=10):
+    sorted_list = sorted(my_dict.items(), key=lambda x: x[1], reverse=True)
     for i in range(top_num):
         print(sorted_list[i])
-    
+
+
 def process_file(filename):
     print('News file: {}'.format(filename))
-    dict = get_dict_from_news_file(filename)    
-    print_top(dict)    
-    
+    my_dict = get_dict_from_news_file(filename)
+    print_top(my_dict)
+
+
 files = ['newsafr.txt', 'newscy.txt', 'newsfr.txt', 'newsit.txt']
 
 for fname in files:
